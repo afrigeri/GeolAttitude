549fce0fb6a2a2015172b76942e670e926830a90
Alessandro Frigeri <alessandro.frigeri@gmail.com>
2026-07-15 10:07:43 +0200

    updated requirements for docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 project = "GeolAttitude"
@@ -28,7 +28,6 @@ extensions = [
 ]
 
 bibtex_bibfiles = ["refs.bib"]
-html_theme = "furo"
 
 autodoc_member_order = "bysource"
 
